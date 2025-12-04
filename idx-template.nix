33f2623a0895fae228906463a58b537bc70d01c2
farieas.kdi <farieas.kdi@gmail.com>
2025-12-04 08:56:19 +0000

    modify

--- idx-template.nix
+++ idx-template.nix
@@ -1,26 +1,8 @@
 { pkgs, ... }: {
   channel = "stable-24.05";
   packages = [ pkgs.nodejs ];
-#   bootstrap = ''
-
-#     npx nativescript create myCoolApp --template vue --path "$out"
-#     mkdir "$out"/.idx
-#     cp ${./dev.nix} "$out"/.idx/dev.nix && chmod +w "$out"/.idx/dev.nix
-#     chmod -R u+w "$out"
-#     cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
-#     cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
-#     chmod -R u+w "$out"
-#     (cd "$out"; npm install --package-lock-only --ignore-scripts)
-#   '';
-bootstrap = ''
-    # 1. Temporarily move to the output directory
-    (cd "$out"
-    # 2. Create the application using "." as the name.
-    #    NativeScript will create the files directly in the current directory ("$out").
-    npx nativescript create . --template vue
-    )
-
-    # 3. Rest of the script remains the same, but now it references files directly in "$out"
+  bootstrap = ''
+    npx nativescript create .mo --template vue --path "$out"
     mkdir "$out"/.idx
     cp ${./dev.nix} "$out"/.idx/dev.nix && chmod +w "$out"/.idx/dev.nix
     chmod -R u+w "$out"
@@ -28,5 +10,6 @@ bootstrap = ''
     cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
     chmod -R u+w "$out"
     (cd "$out"; npm install --package-lock-only --ignore-scripts)
-'';
+  '';
+
 }
