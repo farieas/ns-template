e09aa6741b942b8f3e9788a494dcf6ab97bca648
farhankdi <43706147+farieas@users.noreply.github.com>
2025-12-09 13:17:35 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
-  channel = "stable-25.05";
+  channel = "stable-24.05";
   packages = [ 
-    pkgs.nodejs_24 
+    pkgs.nodejs
     pkgs.python313
     pkgs.python313Packages.pip
     pkgs.python313Packages.fastapi
