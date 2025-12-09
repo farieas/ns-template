ef86392568a68a2b6f3a86cfa87a5e9b42f79d14
farhankdi <43706147+farieas@users.noreply.github.com>
2025-12-09 13:59:31 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -6,7 +6,7 @@
    ];
     bootstrap = ''
     mkdir -p "$WS_NAME"
-    npm config set legacy-peer-deps true
+    npm config set --legacy-peer-deps true
     npm install nativescript 
     ./node_modules/nativescript/bin/ns create "$WS_NAME" --svelte
     mkdir -p "$WS_NAME/.idx/"
@@ -29,7 +29,7 @@
 
   #   chmod -R u+w "$out"
   #
-  #   cd "$out"; npm install --package-lock-only --ignore-scripts --legacy-peer-deps
+  #   cd "$out"; npm install --package-lock-only --ignore-scripts --legacy-peer-deps true
   # '';
 #   bootstrap = ''
     
